6ce81aaba2cae7bc7a803831abba8358a681e2e3
sabinMas <202682616+sabinMas@users.noreply.github.com>
2026-01-14 19:20:40 +0000

    took on the challenge of using the held function instead of pressed function to change colors of background, the background is set as a variable and when a button is held, the background changes colors, when the button is decompressed the background reverts to the original color

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,27 @@
 #include <bn_backdrop.h>
 #include <bn_color.h>
 #include <bn_core.h>
+#include <bn_keypad.h>
 int main()
 {
     bn::core::init();
-    bn::backdrop::set_color(bn::color(12, 8, 19));
+    // setting the background color as a variable
+    bn::color bg_color(12, 8, 19);
 
+    // trying to make it so when a button is held it shows the color instead of pressed
     while (true)
     {
+        bn::color current_color = bg_color;
+        if (bn::keypad::a_held())
+        {
+            current_color = bn::color(0, 18, 0);
+        }
+        if (bn::keypad::b_held())
+        {
+            current_color = (bn::color(12, 0, 3));
+        }
+        bn::backdrop::set_color(current_color);
+
         bn::core::update();
     }
 }
