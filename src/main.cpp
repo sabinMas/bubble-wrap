6ccce190456b5c1f4ef44341a7f4515d42484541
sabinMas <202682616+sabinMas@users.noreply.github.com>
2026-01-17 19:39:03 +0000

    added sprite to screen

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,16 @@
 #include <bn_color.h>
 #include <bn_core.h>
 #include <bn_keypad.h>
+#include <bn_sprite_ptr.h>
+#include <bn_sprite_items_dot.h>
 int main()
 {
     bn::core::init();
     // setting the background color as a variable
     bn::color bg_color(12, 8, 19);
+
+    // adding sprites to screen
+    bn::sprite_ptr myCircle = bn::sprite_items::dot.create_sprite(10, 40);
 
     // trying to make it so when a button is held it shows the color instead of pressed
     while (true)
